2db91278246579f2d795d525a82edd9e0ef0fe01
Lyndon Sanche <lsanche@lyndeno.ca>
2021-03-13 18:21:20 -0700

    Initial stack work

--- src/impl/x86_64/boot/main.asm
+++ src/impl/x86_64/boot/main.asm
@@ -3,6 +3,12 @@ global start
 section .text
 bits 32
 start:
+    mov esp, stack_top ; move stack pointer to top of stack
     ; print 'OK'
     mov dword [0xb8000], 0x2f4b2f4f
     hlt
+
+section .bss
+stack_bottom:
+    resb 4096 * 4
+stack_top:
